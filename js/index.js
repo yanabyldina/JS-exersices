3da559321ef36eb573a80a18338eeb3b7d8003af
Yana Byldina <yanabyldina@gmail.com>
2019-01-18 16:39:01 +0000

    Project Created

---
+++ js/index.js
@@ -0,0 +1,21 @@
+let login = prompt ("Login");
+if (login== "admin") {
+    alert ("Password?") ;
+} 
+else if ("отмена") {
+    alert ("Вход отменен"); 
+}
+else {
+    alert ("Я вас не знаю"); 
+}
+
+let password = prompt ("Password?")
+if ("отмена") {
+    alert ("Вход отменен") ;
+}
+else if (1202) {
+    alert ("Добро пожаловать") ;
+}
+else {
+    alert("Пароль неверный");
+}
